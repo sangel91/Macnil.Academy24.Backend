6b486c3d3eb3b70c5a5ac7a0eae7f03789a85f4e
sergio.angelastro@macnil.it-at-093874331839 <sergio.angelastro@macnil.it>
2024-11-11 12:00:28 +0000

    fix post review

--- src/main/java/macnil/academy/controller/NewsRestController.java
+++ src/main/java/macnil/academy/controller/NewsRestController.java
@@ -1,15 +1,14 @@
 package macnil.academy.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import macnil.academy.model.News;
 import macnil.academy.repository.NewsRepository;
@@ -17,39 +16,33 @@ import macnil.academy.service.NewsService;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/v1/news")
+@RequestMapping("/api/v1")
 public class NewsRestController {
-
 
     @Autowired
     NewsRepository newsRepository;
 
-    @Autowired 
+    @Autowired
     NewsService newsService;
 
-
-   
-
-    @GetMapping (value = "/{newsId}")
-    public News read(@PathVariable ("id") Long id){
-         News news = newsService.read(id);
-         return news;
+    @GetMapping(value = "/{newsId}")
+    public News read(@PathVariable("id") Long id) {
+        News news = newsService.read(id);
+        return news;
     }
 
+    @GetMapping("tenants/{tenantId}/news")
+    public List<News> readAll(@PathVariable("tenantId") Long tenantId) { // @RequestBody // required =
+                                                                         // false,@RequestParam( name="tenantId")
 
-    
-
-    @GetMapping("tenants/{tenantId}")
-    public  List <News> readAll( @PathVariable ("tenantId") Long tenantId ){  //@RequestBody  //  required = false,@RequestParam( name="tenantId") 
-      
-        List <News> news = newsService.readAll(tenantId);
+        List<News> news = newsService.readAll(tenantId);
 
         return news;
 
     }
 
-        @GetMapping
-        public @ResponseBody List<News> getAllNews() {
+    @GetMapping
+    public @ResponseBody List<News> getAllNews() {
 
         return newsRepository.findAll();
 
