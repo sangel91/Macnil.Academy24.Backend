cca8771fa275840a35e601cdfe16f29e8a69ddfb
dimat130 <fedeledimattia2001@gmail.com>
2024-11-11 08:38:20 +0000

    ciao

--- src/main/java/macnil/academy/controller/EntryController.java
+++ src/main/java/macnil/academy/controller/EntryController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/entry")
+@RequestMapping("api/v1/gitentry")
 public class EntryController {
 
     @Autowired
